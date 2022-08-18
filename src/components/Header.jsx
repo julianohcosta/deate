4aecd9d7e453803dbc10e77e52b681f3921fc2df
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-08-18 15:32:55 -0400

    alinha header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,16 @@
 import classes from "./Header.module.css";
-import {MenuOutlined} from "@ant-design/icons";
+import { MenuOutlined } from "@ant-design/icons";
 
 const Header = props => {
   return (
-    <header className={classes['main-header']}>
-      <div>
-        <MenuOutlined className={classes['hamburguer-menu']} onClick={props.onClick}/>
-        <span className={classes['header-text']}>
-                Gerencial DEATE
-            </span>
-      </div>
+    <header className={classes["main-header"]}>
+      <MenuOutlined
+        className={classes["hamburguer-menu"]}
+        onClick={props.onClick}
+      />
+      <span className={classes["header-text"]}>Gerencial DEATE</span>
     </header>
-  )
+  );
 };
 
 export default Header;
