0964efb762faa2180aa85d71fb6d4a98cbf64d55
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-08-18 11:38:13 -0400

    excluir maincontainer

--- src/pages/EstoqueMensal.jsx
+++ src/pages/EstoqueMensal.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-import MainContainer from "../components/UI/MainContainer";
 import classes from "./EstoqueMensal.module.css";
 
 const EstoqueMensal = () => {
   return (
-    <MainContainer>
+    <>
       <div className={classes["container-estoque"]}>
         <div className={classes["container-option-bar"]}>optionBar</div>
         <div className={classes["container-equipes"]}>Equipes</div>
         <div className={classes["container-deate"]}>Deate</div>
         <div className={classes["container-btn"]}>Button</div>
       </div>
-    </MainContainer>
+    </>
   );
 };
 
