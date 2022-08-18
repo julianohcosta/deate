98ddedb98ce7d48b971d36ad1833533f0dfa3001
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2022-08-18 11:23:18 -0400

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import classes from "./Header.module.css";
 import {MenuOutlined} from "@ant-design/icons";
 
 const Header = props => {
-    return (
-        <header className={classes['main-header']}>
-            <div >
-                <MenuOutlined className={classes['hamburguer-menu']} onClick={props.onClick}/>
-                <span className={classes['header-text']}>
-                HEADER
+  return (
+    <header className={classes['main-header']}>
+      <div>
+        <MenuOutlined className={classes['hamburguer-menu']} onClick={props.onClick}/>
+        <span className={classes['header-text']}>
+                Gerencial DEATE
             </span>
-            </div>
-        </header>
-    )
+      </div>
+    </header>
+  )
 };
 
 export default Header;
