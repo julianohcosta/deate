c342ab03d195ac294cfc706d72c9ef1fe67a6286
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-08-18 11:32:05 -0400

    inicia implemtacao estoque

--- src/pages/EstoqueMensal.jsx
+++ src/pages/EstoqueMensal.jsx
@@ -1,43 +1,19 @@
-import {Button, Form, Input} from 'antd';
+import React from "react";
+
+import MainContainer from "../components/UI/MainContainer";
+import classes from "./EstoqueMensal.module.css";
 
 const EstoqueMensal = () => {
-
-    const {TextArea} = Input;
-
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-    };
-
-    return (
-        <div style={{
-            marginLeft: '4em'
-        }}>
-            <Form
-                name="estoque-form"
-                onFinish={onFinish}
-            >
-                <Form.Item label="Deates" style={{
-                    fontWeight: '600',
-                }}>
-                    <TextArea rows={10} style={{
-                        width: '15em',
-                        resize: 'none',
-                    }}/>
-                </Form.Item>
-                <Form.Item label=" " colon={false}>
-                    <Button type="primary" htmlType="submit"
-                            style={{
-                                borderRadius: '.425em',
-                                backgroundColor: '#6184c0',
-                            }}
-                    >
-                        Gerar
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-
-    )
+  return (
+    <MainContainer>
+      <div className={classes["container-estoque"]}>
+        <div className={classes["container-option-bar"]}>optionBar</div>
+        <div className={classes["container-equipes"]}>Equipes</div>
+        <div className={classes["container-deate"]}>Deate</div>
+        <div className={classes["container-btn"]}>Button</div>
+      </div>
+    </MainContainer>
+  );
 };
 
 export default EstoqueMensal;
