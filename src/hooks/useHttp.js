70f3cdf9f9c65bb419caafd5cab2ad81a95faacc
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2022-08-19 16:28:57 -0400

    Fixes http requests

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,47 @@
+const {useState} = require("react");
+
+
+const useHttp = (requestConfig, callback) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const sendRequest = () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      fetch(
+        requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+          signal
+        }
+      )
+        .then(response => {
+          if (!response.ok) {
+            setError('Request Failed!');
+          }
+          return response.json();
+        }).then(resposta => callback(resposta)
+      ).catch(err => {
+        setError(err.message || 'Something went wrong!')
+      })
+
+    } catch (e) {
+      setError(e.message || 'Something went wrong!')
+    }
+    setIsLoading(false);
+  };
+  return {
+    isLoading,
+    error,
+    sendRequest,
+    controller
+  }
+};
+
+export default useHttp;
