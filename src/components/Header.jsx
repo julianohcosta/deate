e4c7b40455b55181f17f47c04905e0d8bcb93944
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2022-08-23 15:32:22 -0400

    logo labin link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,13 +39,19 @@ const Header = props => {
       </Col>
       <Col span={3} align={"center"}>
         <div className={classes["logo-container"]}>
-          <img
-            className={classes.logolabin}
-            src={
-              "https://edicao.intranet.receita.fazenda/administracao/rf01/estrutura-organizacional/superintendencia-regional/gabinete/labin01/imagens/logolabinnobg387x649/image_preview"
-            }
-            alt="Awesome Labin01 logo"
-          />
+          <a
+            href="https://rfbgov.sharepoint.com/sites/Labin01/SitePages/Labin01.aspx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className={classes.logolabin}
+              src={
+                "https://edicao.intranet.receita.fazenda/administracao/rf01/estrutura-organizacional/superintendencia-regional/gabinete/labin01/imagens/logolabinnobg387x649/image_preview"
+              }
+              alt="Awesome Labin01 logo"
+            />
+          </a>
         </div>
       </Col>
     </Row>
